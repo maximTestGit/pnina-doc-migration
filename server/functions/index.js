078a91b7840c58d66a726172bcbd8271d20025e9
maximTestGit <32741254+maximTestGit@users.noreply.github.com>
2025-11-17 18:17:38 +0200

    Create index.js

---
+++ server/functions/index.js
@@ -0,0 +1,16 @@
+/**
+ * Google Cloud Functions (Gen 2) - Entry Point
+ * 
+ * This file contains placeholder exports for Cloud Functions.
+ * Actual implementation will be added later.
+ */
+
+// Placeholder HTTP function
+exports.helloWorld = async (req, res) => {
+    // TODO: Implement function logic
+};
+
+// Placeholder event-driven function
+exports.processEvent = async (event) => {
+    // TODO: Implement function logic
+};
